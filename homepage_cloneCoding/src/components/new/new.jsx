fcac9c860c1806be2c47bc1d70b06beff98c0368
mollani0704 <mollani0704@gmail.com>
2022-06-03 18:13:09 +0900

    [Update] 리액트 홈페이지 클론코딩 nav에 들어갈 아이템 컴포넌트 제작
    nav에 들어갈 아이템 컴포넌트 만듦

--- homepage_cloneCoding/src/components/new/new.jsx
+++ homepage_cloneCoding/src/components/new/new.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Footer from '../../footer';
 import BlackBar from '../blackBar/blackBar';
+import Item from '../item';
+import styles from './new.module.css';
 
 const New = () => {
     return (
         <>
             <BlackBar />
-            <div>New</div>
+            <h3 className={styles.title}>HOME / NEW</h3>
+            <img
+                className={styles.new_Img}
+                src={require('../../Img/mainImg.jpg')}
+                alt=""
+            />
+            <div className={styles.contents}>
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+            </div>
             <Footer />
         </>
     );
