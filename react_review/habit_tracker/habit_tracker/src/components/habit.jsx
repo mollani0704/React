a130be3539362364894e0c015020c3f07ea6d233
mollani0704 <mollani0704@gmail.com>
2022-07-20 19:14:15 +0900

    [Add] Habits 컴포넌트 생성 및 habit 컴포넌트와 연결

--- react_review/habit_tracker/habit_tracker/src/components/habit.jsx
+++ react_review/habit_tracker/habit_tracker/src/components/habit.jsx
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
 
 class Habit extends Component {
+    state = {
+        count: 0,
+    };
+
+    handleIncremet = event => {
+        // state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야 함.
+        this.setState({count: this.state.count + 1});
+    };
+
+    handleDecrease = event => {
+        // state 오브젝트 안에 있는 count를 감소 한뒤 state를 업데이트 해야 함.
+        const count = this.state.count - 1;
+        this.setState({count: count < 0 ? 0 : count});
+    };
+
     render() {
+        const {name, count} = this.props.habit;
         return (
             <>
                 <li className="habit">
-                    <span className="habit-name">Reading</span>
-                    <span className="habit-count">8</span>
-                    <button className="habit-button habit-increase">
+                    <span className="habit-name">{name}</span>
+                    <span className="habit-count">{count}</span>
+                    <button className="habit-button habit-increase" onClick={this.handleIncremet}>
                         <i className="fa-solid fa-square-plus"></i>
                     </button>
-                    <button className="habit-button habit-decrease">
+                    <button className="habit-button habit-decrease" onClick={this.handleDecrease}>
                         <i className="fa-solid fa-square-minus"></i>
                     </button>
                     <button className="habit-button habit-delete">
