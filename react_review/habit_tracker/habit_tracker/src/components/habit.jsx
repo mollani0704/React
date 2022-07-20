96088990b202584f653d0be6a978804d9f9fc06c
mollani0704 <mollani0704@gmail.com>
2022-07-20 16:58:01 +0900

    [Add] Habit 컴포넌트 만들고 기본적인 스타일링 추가

---
+++ react_review/habit_tracker/habit_tracker/src/components/habit.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Habit extends Component {
+    render() {
+        return (
+            <>
+                <li className="habit">
+                    <span className="habit-name">Reading</span>
+                    <span className="habit-count">8</span>
+                    <button className="habit-button habit-increase">
+                        <i className="fa-solid fa-square-plus"></i>
+                    </button>
+                    <button className="habit-button habit-decrease">
+                        <i className="fa-solid fa-square-minus"></i>
+                    </button>
+                    <button className="habit-button habit-delete">
+                        <i className="fa-solid fa-trash-can"></i>
+                    </button>
+                </li>
+            </>
+        );
+    }
+}
+
+export default Habit;
