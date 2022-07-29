185327e52d81a5f15235b1c40ab0e4d69abe36f1
mollani0704 <mollani0704@gmail.com>
2022-07-29 17:27:55 +0900

    [Add] Youtube api를 이용해 데이터 받아오고 title 화면에 출력

--- react_review/youtube/src/App.js
+++ react_review/youtube/src/App.js
@@ -1,9 +1,28 @@
+import {useEffect, useState} from 'react';
 import './App.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+    const [videos, setVideos] = useState([]);
+
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+    };
+
+    useEffect(() => {
+        fetch(
+            'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBFrq_zKa-9dJg9qbTU8lsFKF36cwTZ63g',
+            requestOptions,
+        )
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }, []);
+
     return (
         <div className="App">
-            <h1>Hello World~!</h1>
+            <VideoList videos={videos} />
         </div>
     );
 }
