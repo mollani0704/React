7cca5091bd624fac847658091aafe840a6806b0c
mollani0704 <mollani0704@gmail.com>
2022-08-05 15:19:13 +0900

    [Add] youtube app.js 리팩토링

--- react_review/youtube/src/components/search_header/search_header.jsx
+++ react_review/youtube/src/components/search_header/search_header.jsx
@@ -1,15 +1,40 @@
 import styles from './search_header.module.css';
-import React from 'react';
+import React, {useRef} from 'react';
 
-const SearchHeader = props => {
+const SearchHeader = ({onSearch}) => {
+    const inputRef = useRef();
+
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        onSearch(value);
+    };
+
+    const onClick = () => {
+        console.log('onCLick');
+        handleSearch();
+    };
+
+    const onKeyPress = event => {
+        console.log('onKEypress');
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
                 <img className={styles.logoImg} src="/images/logo.png" alt="logo" />
                 <h1 className={styles.title}>Youtube</h1>
             </div>
-            <input className={styles.input} type="search" placeholder="Search...." />
-            <button className={styles.button} type="submit">
+            <input
+                ref={inputRef}
+                className={styles.input}
+                type="search"
+                placeholder="Search...."
+                onKeyPress={onKeyPress}
+            />
+            <button className={styles.button} type="submit" onClick={onClick}>
                 <img className={styles.buttonImg} src="/images/search.png" alt="" />
             </button>
         </div>
--- react_review/youtube/src/index.js
+++ react_review/youtube/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '@fortawesome/fontawesome-free/js/all.js';
+import Youtube from './service/youtube';
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <App youtube={youtube} />
     </React.StrictMode>,
 );
